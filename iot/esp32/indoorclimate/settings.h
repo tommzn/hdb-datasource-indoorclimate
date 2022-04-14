cad555c00b7a1a9616dff14c5506fa21b0e2309e
Thomas Schenker <tommzn@gmx.de>
2022-04-14 22:57:06 +0200

    add settings files

---
+++ iot/esp32/indoorclimate/settings.h
@@ -0,0 +1,13 @@
+// Sleep settings
+#define uS_TO_S_FACTOR 1000000
+#define SECONDS_TO_SLEEP  10
+
+// Max reconnect attemps for Wifi and AWS IOT connections
+#define MAX_RECONNECT_ATTEMPS 10
+
+// AWS IOT settings
+const char AWS_IOT_THING_NAME[] = "ThingName";
+const char AWS_IOT_ENDPOINT[]   = "xyz-ats.iot.us-west-1.amazonaws.com";
+const char AWS_IOT_TOPIC[]      = "iot/topic";
+
+  
