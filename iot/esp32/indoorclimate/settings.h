c10305399088e214fbcdee29d60bf3ffb40ddeba
Thomas Schenker <tommzn@gmx.de>
2022-04-16 21:29:05 +0200

    Refactor MQTT publisher

--- iot/esp32/indoorclimate/settings.h
+++ iot/esp32/indoorclimate/settings.h
@@ -1,9 +1,17 @@
-// Sleep settings
 #define uS_TO_S_FACTOR 1000000
-#define SECONDS_TO_SLEEP  10
+#define mS_TO_S_FACTOR 1000
 
-// Max reconnect attemps for Wifi and AWS IOT connections
-#define MAX_RECONNECT_ATTEMPS 10
+// Interval for BLE sensor scan, current 10m
+#define SECONDS_TO_SLEEP 600
+
+// Switch display to sleep mode after 5s
+#define DISPLAY_TIMEOUT 5
+
+// Max Wifi connect attemps
+#define MAX_WIFI_CONNECT_ATTEMPS 10
+
+// Max AWS IOT connect attemps
+#define MAX_AWSIOT_CONNECT_ATTEMPS 10
 
 // AWS IOT settings
 const char AWS_IOT_THING_NAME[] = "ThingName";
