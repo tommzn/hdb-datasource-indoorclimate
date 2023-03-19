124adccb20a7f2ba50d961e0df9f3e1f4df075e8
Thomas Schenker <tommzn@gmx.de>
2023-03-19 17:23:59 +0100

    add k8s collector

---
+++ k8s/go.mod
@@ -0,0 +1,12 @@
+module github.com/tommzn/hdb-datasource-indoorclimate/k8s
+
+go 1.16
+
+require (
+	github.com/tommzn/go-config v1.0.5
+	github.com/tommzn/go-log v1.1.0
+	github.com/tommzn/go-secrets v1.1.1
+	github.com/tommzn/hdb-datasource-core v1.2.2
+	github.com/tommzn/hdb-datasource-indoorclimate v1.6.7
+	github.com/tommzn/hdb-datasource-indoorclimate/targets v1.0.6 // indirect
+)
---
+++ k8s/main.go
@@ -0,0 +1,77 @@
+package main
+
+import (
+	"context"
+	"os"
+
+	config "github.com/tommzn/go-config"
+	log "github.com/tommzn/go-log"
+	secrets "github.com/tommzn/go-secrets"
+
+	core "github.com/tommzn/hdb-datasource-core"
+	indoorclimate "github.com/tommzn/hdb-datasource-indoorclimate"
+	targets "github.com/tommzn/hdb-datasource-indoorclimate/targets"
+)
+
+func main() {
+
+	ctx := context.Background()
+	collector, err := bootstrap(ctx)
+	if err != nil {
+		panic(err)
+	}
+	collector.Run(ctx)
+}
+
+// bootstrap loads config and creates a new scheduled collector with a exchangerate datasource.
+func bootstrap(ctx context.Context) (core.Collector, error) {
+
+	secretsManager := newSecretsManager()
+	conf := loadConfig()
+	logger := newLogger(conf, secretsManager, ctx)
+	datasource := indoorclimate.NewMqttCollector(conf, logger)
+	if queue := conf.Get("hdb.queue", nil); queue != nil {
+		datasource.AppendTarget(targets.NewSqsTarget(conf, logger))
+	}
+	if timestreamTable := conf.Get("aws.timestream.table", nil); timestreamTable != nil {
+		datasource.AppendTarget(targets.NewTimestreamTarget(conf, logger))
+	}
+	return core.NewContinuousCollector(datasource, logger), nil
+}
+
+// loadConfig from config file.
+func loadConfig() config.Config {
+
+	configSource, err := config.NewS3ConfigSourceFromEnv()
+	if err != nil {
+		panic(err)
+	}
+
+	conf, err := configSource.Load()
+	if err != nil {
+		panic(err)
+	}
+	return conf
+}
+
+// newSecretsManager retruns a new container secrets manager
+func newSecretsManager() secrets.SecretsManager {
+	secretsManager := secrets.NewDockerecretsManager("/run/secrets/token")
+	secrets.ExportToEnvironment([]string{"AWS_ACCESS_KEY_ID", "AWS_SECRET_ACCESS_KEY"}, secretsManager)
+	return secretsManager
+}
+
+// newLogger creates a new logger from  passed config.
+func newLogger(conf config.Config, secretsMenager secrets.SecretsManager, ctx context.Context) log.Logger {
+	logger := log.NewLoggerFromConfig(conf, secretsMenager)
+	logContextValues := make(map[string]string)
+	logContextValues[log.LogCtxNamespace] = "hdb-datasource-indoorclimate-mqtt"
+	if node, ok := os.LookupEnv("K8S_NODE_NAME"); ok {
+		logContextValues[log.LogCtxK8sNode] = node
+	}
+	if pod, ok := os.LookupEnv("K8S_POD_NAME"); ok {
+		logContextValues[log.LogCtxK8sPod] = pod
+	}
+	logger.WithContext(log.LogContextWithValues(ctx, logContextValues))
+	return logger
+}
